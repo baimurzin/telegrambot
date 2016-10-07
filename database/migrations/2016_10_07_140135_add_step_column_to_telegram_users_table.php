61dbc4e0fb95bca1718aa78a75cc211091cdb4db
Vladislav Baimurzin <vbaymurzin@dataarmor.ru>
2016-10-07 17:02:31 +0300

    migration added step to table

---
+++ database/migrations/2016_10_07_140135_add_step_column_to_telegram_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStepColumnToTelegramUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('telegram_users', function (Blueprint $table) {
+            $table->integer('step')->default(0)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('telegram_users', function (Blueprint $table) {
+            $table->dropColumn('step');
+        });
+    }
+}
